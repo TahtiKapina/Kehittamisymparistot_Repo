c452dd55943e3e99c7ea8eaba2c7a35abc10d3bd
Daniel <s2401272@edu.bc.fi>
2026-02-10 14:36:03 +0200

    ScreenSaver saatu toimimaan
    ScreenSaver saatu toimimaan

--- ScreenSaver/Program.cs
+++ ScreenSaver/Program.cs
@@ -2,6 +2,7 @@
 {
     using System.Numerics;
     using Raylib_cs;
+
     internal class Program
     {
         static void Main(string[] args)
@@ -9,9 +10,8 @@
             int screen_width = 800;
             int screen_height = 800;
 
-           
-
             Raylib.InitWindow(screen_height, screen_width, "ScreenSaver");
+            Raylib.SetTargetFPS(30);
 
             Vector2 A = new Vector2(screen_width / 2, 40);
             Vector2 B = new Vector2(40, screen_height / 2);
@@ -22,13 +22,61 @@
             Vector2 dirC = new Vector2(-1, 1);
 
             float speed = 200.0f;
+            float radius = 6.0f;
 
             while (Raylib.WindowShouldClose() == false)
             {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Raylib_cs.Color.Black);
 
+                float dt = Raylib.GetFrameTime();
+                A += dirA * speed * 3 * dt;
+                B += dirB * speed * dt;
+                C += dirC * speed * dt;
+
+                (A, dirA) = Bounce(A, dirA, radius, screen_width, screen_height);
+                (B, dirB) = Bounce(B, dirB, radius, screen_width, screen_height);
+                (C, dirC) = Bounce(C, dirC, radius, screen_width, screen_height);
+
+                Raylib.DrawCircleV(A, (int)radius, Raylib_cs.Color.Red);
+                Raylib.DrawCircleV(B, (int)radius, Raylib_cs.Color.Blue);
+                Raylib.DrawCircleV(C, (int)radius, Raylib_cs.Color.Green);
+
+                Raylib.DrawLineV(A, B, Raylib_cs.Color.Purple);
+                Raylib.DrawLineV(B, C, Raylib_cs.Color.SkyBlue);
+                Raylib.DrawLineV(C, A, Raylib_cs.Color.Brown);
+
+                Raylib.EndDrawing();
             }
 
             Raylib.CloseWindow();
+        }
+
+        private static (Vector2 pos, Vector2 dir) Bounce(Vector2 pos, Vector2 dir, float radius, int screenWidth, int screenHeight)
+        {
+            if (pos.X - radius <= 0.0f)
+            {
+                pos.X = radius;
+                dir.X = -dir.X;
+            }
+            else if (pos.X + radius >= screenWidth)
+            {
+                pos.X = screenWidth - radius;
+                dir.X = -dir.X;
+            }
+
+            if (pos.Y - radius <= 0.0f)
+            {
+                pos.Y = radius;
+                dir.Y = -dir.Y;
+            }
+            else if (pos.Y + radius >= screenHeight)
+            {
+                pos.Y = screenHeight - radius;
+                dir.Y = -dir.Y;
+            }
+
+            return (pos, dir);
         }
     }
 }
